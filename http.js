4c217ed4ec2006a938299592bbb3e830de21b166
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-11-26 19:30:08 -0600

    Added HTTP server creation and response handling code

--- http.js
+++ http.js
@@ -1,3 +1,10 @@
 const http = require("node:http");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  console.log("request received");
+  res.end("Hola mundo");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port localhost:3000");
+});
